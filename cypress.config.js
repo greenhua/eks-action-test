d74a00d3cf9b2d1a37f338f0e815db8f31402a23
greenhua <greenh@gmail.com>
2025-09-08 15:05:41 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -6,3 +6,22 @@ module.exports = defineConfig({
     supportFile: false
   }
 });
+
+const reportportal = require('reportportal-agent-js-cypress');
+
+module.exports = {
+  e2e: {
+    setupNodeEvents(on, config) {
+      reportportal(on, config, {
+        token: 'test01_n-Y5rdGESZe1qIgv2lIiZqad9-N_8_uxE8E32D3fbiptV-pHdnyNhdFbHWp0MmeN/,
+        endpoint: process.env.https://rp.bfx.elegenbio.com/,
+        launch: 'Cypress Tests',
+        project: 'eks-test',
+        description: 'TSEST wikipedia.org'
+      });
+      return config;
+    },
+    baseUrl: 'https://wikipedia.org',
+    supportFile: false
+  }
+};

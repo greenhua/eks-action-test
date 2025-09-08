88e27cb6ac5e3cfd9adc55b93094e44271a2e4ea
greenhua <greenh@gmail.com>
2025-09-08 19:53:51 +0300

    Update run-tests2.sh

--- run-tests2.sh
+++ run-tests2.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-echo "----------------------------------"
-set -x
+
 echo "Starting Cypress tests..."
 
+npm run test
+
+echo "Uploading to ReportPortal..."
+npm run upload-rp
+
+echo "All tasks completed successfully!"
